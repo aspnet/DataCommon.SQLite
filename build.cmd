9d05716b4f35461d927ed006b4824012fb46f836
Nate McMaster <nate.mcmaster@microsoft.com>
2015-09-15 17:57:37 -0700

    Add x64 Windows dll and run tests against x64

--- build.cmd
+++ build.cmd
@@ -4,7 +4,10 @@ cd %~dp0
 SETLOCAL
 SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
 SET BUILDCMD_KOREBUILD_VERSION=""
-SET BUILDCMD_DNX_VERSION=""
+IF "%BUILDCMD_DNX_VERSION%"=="" (
+    SET BUILDCMD_DNX_VERSION="latest"
+)
+
 
 IF EXIST %CACHED_NUGET% goto copynuget
 echo Downloading latest version of NuGet.exe...
@@ -17,7 +20,7 @@ md .nuget
 copy %CACHED_NUGET% .nuget\nuget.exe > nul
 
 :restore
-IF EXIST packages\KoreBuild goto run
+IF EXIST packages\KoreBuild goto dnx
 IF %BUILDCMD_KOREBUILD_VERSION%=="" (
 	.nuget\NuGet.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
 ) ELSE (
@@ -25,13 +28,13 @@ IF %BUILDCMD_KOREBUILD_VERSION%=="" (
 )
 .nuget\NuGet.exe install Sake -ExcludeVersion -Source https://www.nuget.org/api/v2/ -Out packages
 
+:dnx
 IF "%SKIP_DNX_INSTALL%"=="1" goto run
-IF %BUILDCMD_DNX_VERSION%=="" (
-	CALL packages\KoreBuild\build\dnvm upgrade -runtime CLR -arch x86
-) ELSE (
-	CALL packages\KoreBuild\build\dnvm install %BUILDCMD_DNX_VERSION% -runtime CLR -arch x86 -a default
-)
-CALL packages\KoreBuild\build\dnvm install default -runtime CoreCLR -arch x86
+
+CALL packages\KoreBuild\build\dnvm install %BUILDCMD_DNX_VERSION% -runtime CLR -arch x86 -a default
+CALL packages\KoreBuild\build\dnvm install %BUILDCMD_DNX_VERSION% -runtime CLR -arch x64
+CALL packages\KoreBuild\build\dnvm install %BUILDCMD_DNX_VERSION% -runtime CoreCLR -arch x86
+CALL packages\KoreBuild\build\dnvm install %BUILDCMD_DNX_VERSION% -runtime CoreCLR -arch x64
 
 :run
 CALL packages\KoreBuild\build\dnvm use default -runtime CLR -arch x86
